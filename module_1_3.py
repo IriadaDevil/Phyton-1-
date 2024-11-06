9090164cb2e6f5b44b8eb11b59f48eb7824be1b7
IriadaDevil <kistdyaboli@gmail.com>
2024-11-06 05:48:33 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name="Polina"
+print("Name:",(str(name)))
+age=25
+print("Age:",(int(age)))
+new_age=3
+print("New Age:",(int(age+new_age)))
+is_student=True
+print("Is Student:",(bool(is_student)))
