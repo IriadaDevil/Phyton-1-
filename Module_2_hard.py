71f7625f6c90efab95b511ac4f298e109f5d000f
IriadaDevil <kistdyaboli@gmail.com>
2024-11-19 05:19:30 +0300

    Add files via upload

--- Module_2_hard.py
+++ Module_2_hard.py
@@ -1,16 +1,14 @@
-def get_matrix(n, m, value):
-    matrix = []
+def get_password(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
 
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)
+n = int(input('Enter an integer from 3 to 20: '))
 
-    return matrix
-
-result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+result = get_password(n)
+print('Password:', result)
